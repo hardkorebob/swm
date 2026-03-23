fdfc50664c6408579006f4f320a99b63d2a29ee7
Roberto Rodriguez Jr <64380209+hardkorebob@users.noreply.github.com>
2026-03-22 21:35:50 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+CONF=$HOME/.config/swm
+DIR=/usr/local/bin
+cc -O2 -o swm swm.c -lX11 -Wall -Wextra -pedantic 
+strip -s swm
+mkdir $CONF 
+cp config $CONF
+chmod +x swmctl swm-session
+sudo cp swmctl swm-session swm /usr/local/bin
+sudo cp swm.desktop /usr/share/xsessions
